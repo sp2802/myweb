b8a10d6a5e4ea281d03b3aa7e95dd6491cdbba67
sp2802 <67074012+sp2802@users.noreply.github.com>
2021-01-02 09:51:17 +0530

    Add project files.

---
+++ myweb/Controllers/Models/contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myweb.Controllers.Models
+{
+    public class contact
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public DateTime dateCreated { get; set; } = DateTime.Now;
+    }
+}
