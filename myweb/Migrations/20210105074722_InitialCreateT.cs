d625de002d36b7165cf3a414fcce0069231e60b9
sp2802 <67074012+sp2802@users.noreply.github.com>
2021-01-06 16:57:13 +0530

    EFcrud

---
+++ myweb/Migrations/20210105074722_InitialCreateT.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace myweb.Migrations
+{
+    public partial class InitialCreateT : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "dateCreated",
+                table: "Contacts",
+                newName: "DateCreated");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Contacts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "Contacts",
+                newName: "dateCreated");
+        }
+    }
+}
